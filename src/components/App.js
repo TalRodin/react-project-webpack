7d93a0a0a28faa080ed98c19bb69683e3e694887
Alyona <alyona.rodin@gmail.com>
2019-11-17 02:18:00 -0500

    added

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react"; 
+import '../styles/App.css'; 
+class App extends Component {    
+   render() {        
+       return (            
+            <div>                
+                  <h1>REACT!</h1>
+            </div>);    
+            }
+} 
+export default App;
